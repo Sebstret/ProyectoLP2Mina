5633d1bd4e45ecf6a3a5fc4766ce0e650d35f02b
Sebstret <130103563+Sebstret@users.noreply.github.com>
2023-04-06 16:39:16 -0500

    Update Cliente.java
    Cambios:
    - Funciones agregadas en base a los requerimientos funcionales
    - Atributos cambiados porque estaban por defecto del UML. Guardar un idCliente no tiene mucho sentido ya que ya se graba un idUsuario, la clase padre. 
    - Agregado un arreglo de Pedidos para gestionar estos mismos segun los requerimientos funcionales.

--- Proyecto_Git/Cliente.java
+++ Proyecto_Git/Cliente.java
@@ -1,16 +1,33 @@
 class Cliente extends Usuario{
-	private int idCliente;
-	private String billingAddress;
-	private String documento;
-	private String defaultShippingAddress;
+	private String direccionDeEntregaPorDefecto;
+	private String ciudad;
+	private String distrito;
+	public ArrayList<Pedido> pedidosRealizados;
 
 	public Cliente(int idUsuario, int DNI, String nombre, String email, String contraseña,
-		int telefono, boolean activo,int idCliente, String billingAddress, String documento, String defaultShippingAddress){
+		int telefono, boolean activo, String direccionDeEntregaPorDefecto, String ciudad, String distrito){
 		super(idUsuario,DNI,nombre,email,contraseña,telefono,activo);
-		this.idCliente=idCliente;
-		this.billingAddress=billingAddress;
-		this.documento=documento;
-		this.defaultShippingAddress=defaultShippingAddress;
+		this.direccionDeEntregaPorDefecto = direccionDeEntregaPorDefecto;
+		this.ciudad = ciudad;
+		this.distrito = distrito;
 	}
-
+	
+	@Override
+	public String consultarDatos(){
+	}
+	
+	public void registrarPedido(){
+	}
+	
+	public int buscarPedido(){
+	}
+	
+	public void actualizarPedido(int idPedido){
+	}
+	
+	public void eliminarPedido(){
+	}
+	
+	public void puntuarVendedor(){
+	}
 }
