0e0b936cd1781cfd9242c5d4fd641a7b10fba984
Sebstret <130103563+Sebstret@users.noreply.github.com>
2023-04-06 21:22:51 -0500

    Update Cliente.java
    Varias funciones nuevas

--- Proyecto_Git/Cliente.java
+++ Proyecto_Git/Cliente.java
@@ -1,16 +1,55 @@
+import java.util.ArrayList;
+
 class Cliente extends Usuario{
-	private int idCliente;
-	private String billingAddress;
-	private String documento;
-	private String defaultShippingAddress;
+	private String direccionDeEntregaPorDefecto;
+	private String ciudad;
+	private String distrito;
+	private ArrayList<Pedido> pedidosRealizados;
 
 	public Cliente(int idUsuario, int DNI, String nombre, String email, String contraseña,
-		int telefono, boolean activo,int idCliente, String billingAddress, String documento, String defaultShippingAddress){
+		int telefono, boolean activo, String direccionDeEntregaPorDefecto, String ciudad, String distrito){
 		super(idUsuario,DNI,nombre,email,contraseña,telefono,activo);
-		this.idCliente=idCliente;
-		this.billingAddress=billingAddress;
-		this.documento=documento;
-		this.defaultShippingAddress=defaultShippingAddress;
+		this.direccionDeEntregaPorDefecto = direccionDeEntregaPorDefecto;
+		this.ciudad = ciudad;
+		this.distrito = distrito;
 	}
-
+	
+	// Getters y Setters respectivos
+	
+	@Override
+	public void consultarDatosPersonales(){
+	}
+	
+	@Override
+	public void editarDatosPersonales(){
+	}
+	
+	@Override
+	public void consultarHistorial(){
+		// En el caso del cliente consulta su historial de compras
+	}
+	
+	public void registrarPedido(Pedido pedido){
+	}
+	
+	public void verPedidos(){
+	}
+	
+	public void confirmarPedidos(){
+	}
+	
+	public void pagarPedido(Pedido pedido){
+	}
+	
+	public void cancelarPedido(Pedido pedido){
+	}
+	
+	public void calificarVendedor(Vendedor vendedor){
+	}
+	
+	public void consultarDatosEnvio(Pedido pedido){
+	}
+	
+	public String emitirReportePedidos(){
+	}
 }
